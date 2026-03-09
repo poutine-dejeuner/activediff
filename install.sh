ade985c2a5f19444b02d0aaee0c32589e891e34f
Vincent Létourneau <59657878+poutine-dejeuner@users.noreply.github.com>
2026-03-09 10:28:52 -0400

    Update install.sh to download nanophotodata
    Add commands to create data directory and download data.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/bash
 conda create -n mp -c conda-forge pymeep
-# conda install gls
 uv venv --system-site-packages
 source .venv/bin/activate
 uv sync --extra dev
-
+mkdir data
+wget -O data/nanophotodata.zip --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1oRCZmrC0aGuYBmA2VRkR9rROH-XtWnd8'
+unzip data/nanophotodata.zip -d data
