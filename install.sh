7d8268bf30578bc17d1ff002a294fee1136c58e1
Vincent <vincentmillions@gmail.com>
2026-03-12 15:16:42 -0400

    debug de install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/bash
-conda create -n mp -c conda-forge pymeep
+cd ~
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source miniconda3/bin/activate
+conda create -n mp python=3.12 conda-forge::pymeep
+conda activate mp
+wget -qO- https://astral.sh/uv/install.sh | sh
+source
+cd activediff
 uv venv --system-site-packages
 source .venv/bin/activate
 uv sync --extra dev
